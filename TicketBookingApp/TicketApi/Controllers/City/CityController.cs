58fc820b87f57cbbb98e35231a104ea32b205a68
Nandhni G A <nandhni.25@gmail.com>
2023-09-23 10:34:28 +0700

    adding city controller to the api project

---
+++ TicketBookingApp/TicketApi/Controllers/City/CityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketApi.Controllers.City
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+    }
+}
